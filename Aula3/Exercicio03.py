4a0fbb505e9ac5b59d00594f419839a0c8845132
Erick-A-Martins <erickalvesqmartins@gmail.com>
2025-03-27 18:47:20 -0300

    add time in exercise 3

--- Aula3/Exercicio03.py
+++ Aula3/Exercicio03.py
@@ -1,15 +1,17 @@
 from PIL import Image
 from tkinter import Tk, filedialog
 import threading
+import time
 
-def processar_faixa(imagem, imagem_pb, inicio, fim):
+def processar_faixa(imagem, imagem_pb, inicio, fim, lock):
     
     largura, _ = imagem.size
     for y in range(inicio, fim):
         for x in range(largura):
             r, g, b = imagem.getpixel((x, y))
             luminancia = int(0.299 * r + 0.587 * g + 0.114 * b)
-            imagem_pb.putpixel((x, y), luminancia)
+            with lock:
+                imagem_pb.putpixel((x, y), luminancia)
 
 def converter_para_preto_e_branco():
     try:
@@ -29,13 +31,18 @@ def converter_para_preto_e_branco():
         
         num_threads = 4  # Numero de threads
         faixas = [(altura * i // num_threads, altura * (i + 1) // num_threads) for i in range(num_threads)]
+        lock = threading.Lock()
+        threads = []
         
-        threads = [threading.Thread(target=processar_faixa, args=(imagem, imagem_pb, inicio, fim)) for inicio, fim in faixas]
-        
-        for thread in threads:
+        tempo_inicio = time.time()
+        for inicio, fim in faixas:
+            thread = threading.Thread(target=processar_faixa, args=(imagem, imagem_pb, inicio, fim, lock))
             thread.start()
+            threads.append(thread)
+        
         for thread in threads:
             thread.join()
+        tempo_fim = time.time()
         
         caminho_saida = filedialog.asksaveasfilename(
             title="Salvar imagem em preto e branco",
@@ -48,7 +55,7 @@ def converter_para_preto_e_branco():
         
         imagem_pb.save(caminho_saida)
         print(f"Imagem convertida com sucesso! Salva em: {caminho_saida}")
-    
+        print(f"Tempo de execução com threads: {tempo_fim - tempo_inicio:.2f} segundos")
     except Exception as e:
         print(f"Erro ao processar a imagem: {e}")
 
